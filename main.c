636ff74f005ad0cea2cbf622b2a8209f949391c9
Twoody <tanner.l.woody@gmail.com>
2014-09-15 20:43:10 -0500

    Boxed off integers_that_divide_me()

--- main.c
+++ main.c
@@ -83,96 +83,97 @@ int is_prime(int x)                                         /**/
 }//End of is_prime(x)                                       /**/
 /**************************************************************/
 
-
-int integers_that_divide_me(int number)
-{
-    head = NULL;
-    int counter, candidates,i,x,tempz,indexed_node,count,bewl;
-     //  candidates will be used as a countdown, counter will be used for loops
-    pool_of_primes(number); //  Creating a list of the primes that compose our number
-    printf("We are seeing what is created from our pool of primes\n\n");
-    curr =  head;
-    while(curr)
-    {
-        printf("%d\n", curr->val);
-        curr = curr->next ;
-    }
-    printf("DONE\n\n");
-/*********************************************************************************************************/
-    item* temp  = head;     //  assigning a temp node to the head of 1st list to find the size of prime pool
-    candidates  = 0;
-    i           = 0;
-    tempz       = 1;
-
-    int length  = 0;
-    while(temp)
-    {
-        for (i=0;i<length;i++){
-            tempz = tempz*2;
-        }
-        candidates  += tempz;
-        length++;
-        temp         = temp->next;  //  move to next node
-        tempz        = 1;
-    }
-/*********************************************************************************************************/
-/*We now have a length*/
-	if (length >=21)        //  If there are to many integers
-	{
-		printf("\nTo many possible candidates for integers that divide our number.\n");
-		return 0;
-	}
-/*********************************************************************************************************/
-	/*nList.append(1): while loop does not provide this in our list*/
-    curr2        = (item2 *)malloc(sizeof(item2));                                      /**/
-    curr2->val   = 1;                                                                   /**/
-    curr2->next  = head2;                                                               /**/
-    head2        = curr2;                                                               /**/
-/*********************************************************************************************************/
-	while (candidates != 0)
-	{
-		x       =   1;
-		bewl    =   1;
-		for (counter=0; counter<=length;counter++)
-		{
-			if(candidates & (1<<counter))
-			{
-			    curr = head;                        /**/
-                count = 0;
-                while (curr)                             /**/
-                {                                                   /**/
-                    if (count == counter)
-                    {                              /**/
-                        indexed_node = curr->val;
-                        x   = 	x * indexed_node;
-                    }                        /**/
-                    count++;                                         /**/
-                    curr    =   curr->next;                         /**/
-                }
-            }
-		}
-        curr2   =   head2;
-        while(curr2)
-        {
-            if(x == curr2->val)
-            {
-                bewl = 0;
-            }
-            curr2 = curr2->next ;
-        }
-        if (bewl == 1)
-        {
-            curr2         = (item2 *)malloc(sizeof(item2));     /**/
-            curr2->val    = x;                               /**/
-            curr2->next   = head2;                             /**/
-            head2         = curr2;
-        }
-		candidates 	    -=	1;
-	}
-/*********************************************************************************************************/
-	printf("\n\nDONE WITH CONSTRUCTING MAIN LIST\n\n");
-	return 1;
-}
+/******************************************************************************************************************/
+int integers_that_divide_me(int number)										/**/
+{														/**/
+    head = NULL;												/**/
+    int counter, candidates,i,x,tempz,indexed_node,count,bewl;							/**/
+     //  candidates will be used as a countdown, counter will be used for loops					/**/
+    pool_of_primes(number); //  Creating a list of the primes that compose our number				/**/
+    printf("We are seeing what is created from our pool of primes\n\n");					/**/
+    curr =  head;												/**/
+    while(curr)													/**/
+    {														/**/
+        printf("%d\n", curr->val);										/**/
+        curr = curr->next ;											/**/
+    }														/**/
+    printf("DONE\n\n");												/**/
+/***************************************************************************************************************/
+    item* temp  = head;     //  assigning a temp node to the head of 1st list to find the size of prime pool	/**/
+    candidates  = 0;												/**/
+    i           = 0;												/**/
+    tempz       = 1;												/**/
+														/**/
+    int length  = 0;												/**/
+    while(temp)													/**/
+    {														/**/
+        for (i=0;i<length;i++){											/**/
+            tempz = tempz*2;											/**/
+        }													/**/
+        candidates  += tempz;											/**/
+        length++;												/**/
+        temp         = temp->next;  //  move to next node							/**/
+        tempz        = 1;											/**/
+    }														/**/
+/***************************************************************************************************************/
+/*We now have a length*/											/**/
+	if (length >=21)        //  If there are to many integers						/**/
+	{													/**/
+		printf("\nTo many possible candidates for integers that divide our number.\n");			/**/
+		return 0;											/**/
+	}													/**/
+/***************************************************************************************************************/
+	/*nList.append(1): while loop does not provide this in our list*/					/**/
+    curr2        = (item2 *)malloc(sizeof(item2));                                      			/**/
+    curr2->val   = 1;                                                                   			/**/
+    curr2->next  = head2;                                                               			/**/
+    head2        = curr2;                                                               			/**/
+/***************************************************************************************************************/
+	while (candidates != 0)											/**/
+	{													/**/
+		x       =   1;											/**/
+		bewl    =   1;											/**/
+		for (counter=0; counter<=length;counter++)							/**/
+		{												/**/
+			if(candidates & (1<<counter))								/**/
+			{											/**/
+			    curr = head;                        						/**/
+                count = 0;											/**/
+                while (curr)                             							/**/
+                {                                                   						/**/
+                    if (count == counter)									/**/
+                    {                              								/**/
+                        indexed_node = curr->val;								/**/
+                        x   = 	x * indexed_node;								/**/
+                    }                        									/**/
+                    count++;                                         						/**/
+                    curr    =   curr->next;                         						/**/
+                }												/**/
+            }													/**/
+		}												/**/
+        curr2   =   head2;											/**/
+        while(curr2)												/**/
+        {													/**/
+            if(x == curr2->val)											/**/
+            {													/**/
+                bewl = 0;											/**/
+            }													/**/
+            curr2 = curr2->next ;										/**/
+        }													/**/
+        if (bewl == 1)												/**/
+        {													/**/
+            curr2         = (item2 *)malloc(sizeof(item2));     						/**/
+            curr2->val    = x;                               							/**/
+            curr2->next   = head2;                             							/**/
+            head2         = curr2;										/**/
+        }													/**/
+		candidates 	    -=	1;									/**/
+	}													/**/
+/***************************************************************************************************************/
+	printf("\n\nDONE WITH CONSTRUCTING MAIN LIST\n\n");							/**/
+	return 1;												/**/
+}														/**/
+/******************************************************************************************************************/
 
 /**************************************************************/
 int pool_of_primes(int x)                                   /**/
